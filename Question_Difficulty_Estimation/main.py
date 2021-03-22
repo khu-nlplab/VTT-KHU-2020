9bbbc962da8c7fef5c06d8ebd9348fbbabc0428e
suhwan yoon <neo_94@naver.com>
2021-03-22 15:23:54 +0900

    input alternate utterance to vid

--- Question_Difficulty_Estimation/main.py
+++ Question_Difficulty_Estimation/main.py
@@ -4,7 +4,7 @@ import torch
 
 def main():
     question = "Why did Sukyung block Jinsang?"
-    utterance = "Good morning. Says who?[SEP]What? Oh man... Well?[SEP]I can't say it is or isn't.[SEP]"
+    utterance = "AnotherMissOh01_001_0079"
 
     bert_config_file = 'checkpoints/bert_config.json'  # bert config file path
     vocab_file = 'checkpoints/vocab.txt'  # bert vocabulary file path
@@ -12,7 +12,7 @@ def main():
     logical_model_path = 'checkpoints/Logical_level_model.bin'  # logic prediction model file path 
 
     model = LevelClassificationModel(bert_config_file, vocab_file, memory_model_path, logical_model_path)
-    memory_level, logic_level = model.predict(question, utterance)
+    memory_level, logic_level = model.predict(question, vid)
 
     print(memory_level) #gold 3
     print(logic_level) #gold 4
